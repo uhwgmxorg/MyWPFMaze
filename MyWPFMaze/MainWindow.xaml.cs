00016fd17943a3f7e20e51bc22308dcff9684e82
uhwgmxorg <uhw@gmx.org>
2013-11-25 16:59:35 +0100

    Start.

---
+++ MyWPFMaze/MainWindow.xaml.cs
@@ -0,0 +1,215 @@
+﻿/******************************************************************************/
+/*                                                                            */
+/*   Program: MyWPFMaze                                                       */
+/*   A nice maze for WPF                                                      */
+/*                                                                            */
+/*   10.11.2013 0.0.0.0 uhwgmxorg Start                                       */
+/*                                                                            */
+/******************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Tools;
+
+namespace MyWPFMaze
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private double Stroke { get; set; }
+        private Color Color { get; set; }
+
+        private double StrokeThicknessBorder { get; set; }
+        private Color ColorBorder { get; set; }
+
+        private double MazeSize { get; set; }
+        private int MazeSizeX { get; set; }
+        private int MazeSizeY { get; set; }
+        private Color MazeColorSquare = Colors.Purple;
+        private double MazeStrokeThicknessSquare = 5;
+        private double MazeSquareSize = 48;
+        private double MazeOffsetX = 25;
+        private double MazeOffsetY = 15;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Grid_Main.Visibility = System.Windows.Visibility.Visible;
+
+            MazeSize = 3;
+            Slider_MazeSize.Value = MazeSize;
+            Init();
+        }
+
+        /// <summary>
+        /// Init
+        /// </summary>
+        private void Init()
+        {
+            ColorBorder = Colors.Gray;
+            StrokeThicknessBorder = 5;
+
+            MazeSizeX = 4 * (int)Math.Round(MazeSize);
+            MazeSizeY = 5 * (int)Math.Round(MazeSize);
+            MazeSquareSize = 92 / MazeSize;
+
+            MazeColorSquare = Colors.Purple;
+            MazeStrokeThicknessSquare = 14/MazeSize;
+            MazeOffsetX = 35;
+            MazeOffsetY = 20;
+        }
+
+        /******************************/
+        /*       Button Events        */
+        /******************************/
+        #region Button Events
+
+        /// <summary>
+        /// Button_Start_Click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_New_Click(object sender, RoutedEventArgs e)
+        {
+            Canvas_Paper.Children.Clear();
+
+            int MazeWidth = MazeSizeX;
+            int MazeHeight = MazeSizeY;
+
+            WPFMaze Maze = new WPFMaze(Canvas_Paper, MazeSizeX, MazeSizeY);
+
+            Maze.ColorSquare = MazeColorSquare;
+            Maze.StrokeThicknessSquare = MazeStrokeThicknessSquare;
+            Maze.SquareSize = MazeSquareSize;
+            Maze.OffsetX = MazeOffsetX;
+            Maze.OffsetY = MazeOffsetY;
+            Maze.Generat((int)RDNumber(0, MazeWidth - 1), (int)RDNumber(0, MazeHeight - 1));
+            Maze.BuildExitSouth(1);
+
+            Maze.Show();
+        }
+
+        /// <summary>
+        /// Button_Clear_Click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Clear_Click(object sender, RoutedEventArgs e)
+        {
+            Canvas_Paper.Children.Clear();
+        }
+
+        #endregion
+        /******************************/
+        /*      Other Events          */
+        /******************************/
+        #region Other Events
+
+        /// <summary>
+        /// Slider_MazeSize_ValueChanged
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Slider_MazeSize_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (Slider_MazeSize != null)
+            {
+                MazeSize = (int)Slider_MazeSize.Value;
+                Init();
+            }
+        }
+
+        #endregion
+        /******************************/
+        /*      Other Functions       */
+        /******************************/
+        #region Other Functions
+
+        /// <summary>
+        /// DrawBorder
+        /// </summary>
+        void DrawBorder()
+        {
+            Color = ColorBorder;
+            Stroke = StrokeThicknessBorder;
+            Canvas_Paper.Width = 436;
+            Canvas_Paper.Height = 510;
+            DrawLine(0 - Stroke / 2, 0, Canvas_Paper.Width + Stroke / 2, 0);
+            DrawLine(Canvas_Paper.Width, 0, Canvas_Paper.Width, Canvas_Paper.Height);
+            DrawLine(Canvas_Paper.Width + Stroke / 2, Canvas_Paper.Height, 0 - Stroke / 2, Canvas_Paper.Height);
+            DrawLine(0, Canvas_Paper.Height, 0, 0);
+        }
+
+        /// <summary>
+        /// DrawLine
+        /// </summary>
+        /// <param name="x1"></param>
+        /// <param name="y1"></param>
+        /// <param name="x2"></param>
+        /// <param name="y2"></param>
+        void DrawLine(double x1, double y1, double x2, double y2)
+        {
+            Line Line = new Line() { X1 = x1, Y1 = y1, X2 = x2, Y2 = y2 };
+            Line.Stroke = new SolidColorBrush(Color);
+            Line.StrokeThickness = Stroke;
+            Canvas_Paper.Children.Add(Line);
+        }
+
+        /// <summary>
+        /// RDNumber
+        /// </summary>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        /// <returns></returns>
+        double RDNumber(double min,double max)
+        {
+            return Tools.StaticTools.RandomDouble(min,max);
+        }
+
+        #endregion
+    }
+
+    /******************************/
+    /*     Converter Classes      */
+    /******************************/
+    public class DoubleToStringConvertor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double d = (int)(double)value;
+            return (string)d.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                double ret = System.Convert.ToDouble((string)value);
+                return ret;
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ MyWPFMaze/WPFMaze.cs
@@ -0,0 +1,164 @@
+﻿using MazeLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows.Controls;
+
+namespace MyWPFMaze
+{
+    class WPFMaze : VMaze
+    {
+
+        public double StrokeThicknessSquare { get; set; }
+        public Color ColorSquare { get; set; }
+        public double SquareSize { get; set; }
+
+        public double OffsetX { get; set; }
+        public double OffsetY { get; set; }
+
+        private int _lineCounter;
+        private Canvas Canvas_Paper { get; set; }
+        private int CursorX { get; set; }
+        private int CursorY { get; set; }
+        private double Stroke { get; set; }
+        private Color Color { get; set; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public WPFMaze(Canvas canvas, int width, int height)
+            : base(width, height)
+        {
+            Canvas_Paper = canvas;
+            _lineCounter = 0;
+            CursorX = 1;
+            CursorY = 1;
+
+
+            ColorSquare = Colors.Purple;
+            StrokeThicknessSquare = 5;
+            SquareSize = 48;
+
+            OffsetX = 25;
+            OffsetY = 15;
+        }
+
+        /// <summary>
+        /// PrintLConerBase
+        /// </summary>
+        public override void PrintLConerBase()
+        {
+            //System.Console.Write("+--");
+
+            Stroke = StrokeThicknessSquare;
+            Color = ColorSquare;
+            double X = XToS(CursorX);
+            double Y = YToS(CursorY);
+            DrawLine(X + Stroke / 2, Y - SquareSize, X - SquareSize - Stroke / 2, Y - SquareSize);
+            CursorX++;
+        }
+
+        /// <summary>
+        /// PrintLConer
+        /// </summary>
+        public override void PrintLConer()
+        {
+            //System.Console.Write("+  ");
+
+            CursorX++;
+        }
+
+        /// <summary>
+        /// PrintRConer
+        /// </summary>
+        public override void PrintRConer()
+        {
+            //System.Console.Write("+");
+
+        }
+
+        /// <summary>
+        /// PrintWall
+        /// </summary>
+        public override void PrintWall()
+        {
+            //System.Console.Write("|");
+
+            Stroke = StrokeThicknessSquare;
+            Color = ColorSquare;
+            double X = XToS(CursorX);
+            double Y = YToS(CursorY);
+            DrawLine(X - SquareSize, Y - SquareSize, X - SquareSize, Y);
+            CursorX++;
+        }
+
+        /// <summary>
+        /// PrintGap
+        /// </summary>
+        public override void PrintGap()
+        {
+            //System.Console.Write(" ");
+
+            CursorX++;
+        }
+
+        /// <summary>
+        /// PrintBoubleGap
+        /// </summary>
+        public override void PrintDoubleGap()
+        {
+            //System.Console.Write("  ");
+
+        }
+
+        /// <summary>
+        /// NextLine
+        /// </summary>
+        public override void NextLine()
+        {
+            //System.Console.Write("\n");
+
+            CursorX = 1;
+            if (++_lineCounter % 2 == 0)  // only every second time!
+                CursorY++;
+        }
+
+        /// <summary>
+        /// DrawLine
+        /// </summary>
+        /// <param name="x1"></param>
+        /// <param name="y1"></param>
+        /// <param name="x2"></param>
+        /// <param name="y2"></param>
+        private void DrawLine(double x1, double y1, double x2, double y2)
+        {
+            Line Line = new Line() { X1 = x1, Y1 = y1, X2 = x2, Y2 = y2 };
+            Line.Stroke = new SolidColorBrush(Color);
+            Line.StrokeThickness = Stroke;
+            Canvas_Paper.Children.Add(Line);
+        }
+
+        /// <summary>
+        /// Transform X coordinate to sreen
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        private double XToS(double x)
+        {
+            return x * SquareSize + OffsetX;
+        }
+
+        /// <summary>
+        /// Transform Y coordinate to sreen
+        /// </summary>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        private double YToS(double y)
+        {
+            return y * SquareSize + OffsetY;
+        }
+    }
+}
